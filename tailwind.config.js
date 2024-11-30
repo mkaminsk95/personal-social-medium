d3a990888a1d753fa1d287b84345b3b05cadeaed
mkaminsk95 <m.kaminsk95@gmail.com>
2024-11-30 10:50:06 +0100

    add Laravel Breeze with Livewire

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        './resources/views/**/*.blade.php',
     ],
     theme: {
         extend: {
@@ -16,5 +15,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [forms],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +8,11 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        mkcert(),
     ],
+    server: {
+        host: true,
+        https: true,
+        port: 5173,
+    },
 });
