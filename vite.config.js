ca4dba0e8947939559d85d338abf8519e138e7f8
mkaminsk95 <m.kaminsk95@gmail.com>
2024-12-04 16:25:26 +0100

    introduce breeze with react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.tsx',
             refresh: true,
         }),
+        react(),
+        mkcert({
+            savePath: './certs'
+        })
     ],
+
+    server: {
+        host: true,
+        https: true,
+        port: 5173,
+    },
 });
